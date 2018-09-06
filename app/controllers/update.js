b39a87700a1cb365368866d6cfbcd185fb6f7d18
zekitel <zeki.tel34@gmail.com>
2018-09-07 00:42:18 +0300

    Update update.js

--- app/controllers/update.js
+++ app/controllers/update.js
@@ -1,29 +1,18 @@
 
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-
-
 export default Controller.extend({
-
-    
     remotingService:service(),
 
     init() {
         this._super(...arguments);
         this.set('item', {});
     },
-
-
     actions: {
         updateStudent: function () {
-            
-
-            this.get('remotingService').postRemoteCall('http://localhost:8080/students/update',this.get('item'),false);
-            
+           
+            this.get('remotingService').postRemoteCall('http://localhost:8080/students/update',this.get('item'),false);          
         }
-
-
     }
     
-
 });
